70897c66149b3efdb333fd0070f8dfc2dbe5235f
Galih Adi Paryanto <galih.paryanto@gmail.com>
2015-07-24 17:04:17 +0800

    Commit New

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+  #get 'sessions/new'
+
+  #get 'sessions/create'
+
+  #get 'sessions/destroy'
+  root 'sessions#new'
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  
+  get '/signup', to: 'users#new'
+  get '/signin', to: 'sessions#new'
+  delete '/signout', to: 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
